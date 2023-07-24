fb93d188a610679c4e6cc0c5802a31d4a18f0c0e
salmafadlabdulrahman <salmafadlabdulrahman@gmail.com>
2023-07-24 19:00:23 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let adviceNum = document.querySelector(".advice-num");
+let advice = document.querySelector(".advice");
+const btn = document.querySelector(".btn-container");
+
+
+btn.addEventListener("click", function () {
+  generateAdvice();
+});
+
+
+async function generateAdvice() {
+  const response = await fetch(
+    "https://api.adviceslip.com/advice"
+  );
+
+  let data = await response.json();
+  const adviceId = data.slip.id;
+  const theAdvice = data.slip.advice;
+
+  adviceNum.textContent = `Advice #${adviceId}`;
+  advice.textContent = `${theAdvice}`;
+}
